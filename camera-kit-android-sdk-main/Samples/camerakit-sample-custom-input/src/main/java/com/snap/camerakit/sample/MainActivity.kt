f70c401b66a84db5ecf3e09d3487b925ace75203
PavelIvasyuk <dotmmoru@gmail.com>
2025-03-06 13:59:55 +0200

    custom-camera-input -> added lens selector

--- camera-kit-android-sdk-main/Samples/camerakit-sample-custom-input/src/main/java/com/snap/camerakit/sample/MainActivity.kt
+++ camera-kit-android-sdk-main/Samples/camerakit-sample-custom-input/src/main/java/com/snap/camerakit/sample/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Size
 import android.view.Choreographer
 import android.view.Surface
 import android.view.TextureView
+import android.view.GestureDetector
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LifecycleOwner
 import com.snap.camerakit.Session
@@ -15,8 +17,8 @@ import com.snap.camerakit.invoke
 import com.snap.camerakit.lenses.LensesComponent
 import com.snap.camerakit.lenses.whenHasFirst
 import java.io.Closeable
-
 import android.Manifest
+import android.animation.ValueAnimator
 import android.content.pm.PackageManager
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
@@ -28,6 +30,8 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import androidx.camera.camera2.interop.Camera2CameraInfo
 import android.hardware.camera2.CameraCharacteristics
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.annotation.OptIn
 import androidx.camera.camera2.interop.ExperimentalCamera2Interop
 
@@ -47,22 +51,28 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
     private val choreographer = Choreographer.getInstance()
     private val closeOnDestroy = mutableListOf<Closeable>()
 
+    private lateinit var buttonLens1: ImageButton
+    private lateinit var buttonLens2: ImageButton
+    private lateinit var buttonLens3: ImageButton
+
+    private var selectedButton: ImageButton? = null
+
+    private val lensIds = listOf(LENS_ID_1, LENS_ID_2, LENS_ID_3)
+    private var currentLensIndex = 0
+
     companion object {
         const val TOKEN = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM5NzA2OTc2LCJzdWIiOiI3MzZkMzM5Yi03NGIyLTRkMjgtYWQ3NS0zYWExMDc2YzI1YzJ-U1RBR0lOR342YTdmOTA2Zi0zYWJjLTRmMWItYjFkYi02OWM0Y2I2ZWIxMDMifQ.YWxF9attURAA0psdgFdeLLAtaqdJPNQM41rpSb2e51Y"
         const val LENS_GROUP_ID = "ed8aeaf7-12fc-4631-893c-de7705b119fc"
         const val LENS_ID_1 = "b93ca434-dce5-46ae-9c26-3e55d7a745e9"
-        const val LENS_ID_2 = "6c8ee7b5-6dff-4d59-9ce0-ea212b4871e7"
+        const val LENS_ID_2 = "c2cd19d9-1592-4297-9322-c1dcae6094da"
         const val LENS_ID_3 = "84cbe1b1-5864-4e7d-b47f-edfdb155e98d"
     }
 
-    // Contract for requesting camera permission
     private val requestCameraPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
             if (granted) {
-                // If the user granted permission, start the camera
                 startCamera()
             } else {
-                // Permission denied — you can show a toast, dialog, etc.
                 Log.e("CameraXApp", "User denied camera permission")
             }
         }
@@ -71,29 +81,27 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        session = Session(context = this) .apply {
-            lenses.repository.observe(
-                LensesComponent.Repository.QueryCriteria.ById(LENS_ID_1, LENS_GROUP_ID)
-            ) { result ->
-                result.whenHasFirst { requestedLens ->
-                    lenses.processor.apply(requestedLens)
-                }
-            }
+        // Initialize buttons
+        buttonLens1 = findViewById<ImageButton>(R.id.button_lens1)
+        buttonLens2 = findViewById<ImageButton>(R.id.button_lens2)
+        buttonLens3 = findViewById<ImageButton>(R.id.button_lens3)
+
+        // Set click listeners
+        buttonLens1.setOnClickListener { onLensButtonClick(buttonLens1, lensIds[0]) }
+        buttonLens2.setOnClickListener { onLensButtonClick(buttonLens2, lensIds[1]) }
+        buttonLens3.setOnClickListener { onLensButtonClick(buttonLens3, lensIds[2]) }
+        session = Session(context = this).apply {
+            applyLens(lensIds[currentLensIndex])
         }
 
-// Determine the initial size of the input stream.
-// (This size can be changed to suit the camera resolution if necessary)
         val inputWidth = 1440
         val inputHeight = 2560
 
-        // Create a SurfaceTexture and configure it
         inputSurfaceTexture = SurfaceTexture(0).apply {
             setDefaultBufferSize(inputWidth, inputHeight)
-            // Be sure to detach from the GL context before using as input for CameraKit
             detachFromGLContext()
         }
 
-        // Creating an input for CameraKit with SurfaceTexture
         val input = inputFrom(
             surfaceTexture = inputSurfaceTexture,
             width = inputWidth,
@@ -105,29 +113,36 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         )
         session.processor.connectInput(input)
 
-        // We create a Surface, which we pass to CameraX (this same surface is associated with inputSurfaceTexture)
         inputSurface = Surface(inputSurfaceTexture)
 
-        // Connect the CameraKit output to TextureView to visualize the processed video stream
         val previewTextureView = findViewById<TextureView>(R.id.camerakit_output_preview)
         val outputCloseable = session.processor.connectOutput(previewTextureView)
         closeOnDestroy.add(outputCloseable)
 
-        // Initializing the Executor for the camera
         cameraExecutor = Executors.newSingleThreadExecutor()
 
-        // Checking if you have permission to use the camera
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             startCamera()
         } else {
-            // Ask request
             requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+
+        // Mark the first button as selected initially
+        onLensButtonClick(buttonLens1, lensIds[0])
+    }
+
+    private fun Session.applyLens(lensId: String) {
+        lenses.repository.observe(
+            LensesComponent.Repository.QueryCriteria.ById(lensId, LENS_GROUP_ID)
+        ) { result ->
+            result.whenHasFirst { requestedLens ->
+                lenses.processor.apply(requestedLens)
+            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        // Close all resources that were open
         closeOnDestroy.forEach { it.close() }
         cameraExecutor.shutdown()
         session.close()
@@ -135,43 +150,62 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         inputSurfaceTexture.release()
     }
 
-    @OptIn(ExperimentalCamera2Interop::class) private fun startCamera() {
+    private fun onLensButtonClick(button: ImageButton, lensId: String) {
+        // Reset the size of the previously selected button
+        selectedButton?.apply {
+            animateButtonSize(this, resources.getDimensionPixelSize(R.dimen.button_size))
+        }
+
+        // Increase the size of the clicked button
+        animateButtonSize(button, resources.getDimensionPixelSize(R.dimen.button_size_selected))
+
+        // Apply the selected lens
+        session.applyLens(lensId)
+
+        // Update the selected button reference
+        selectedButton = button
+    }
+
+    private fun animateButtonSize(button: ImageButton, targetSize: Int) {
+        val startSize = button.width
+        val animator = ValueAnimator.ofInt(startSize, targetSize)
+        animator.addUpdateListener { valueAnimator ->
+            val animatedValue = valueAnimator.animatedValue as Int
+            button.layoutParams.width = animatedValue
+            button.layoutParams.height = animatedValue
+            button.requestLayout()
+        }
+        animator.duration = 200 // Animation duration in milliseconds
+        animator.start()
+    }
+
+    @OptIn(ExperimentalCamera2Interop::class)
+    private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener({
             val cameraProvider = cameraProviderFuture.get()
 
-            // Create Preview use case
             val preview = Preview.Builder().build()
 
-            // use SurfaceProvider to share inputSurface
             preview.setSurfaceProvider { request ->
-                // Update buffer size SurfaceTexture according to the requirements of CameraX
                 val resolution: Size = request.resolution
                 inputSurfaceTexture.setDefaultBufferSize(resolution.width, resolution.height)
-
-                // Passing the existing surface into the request
-                request.provideSurface(inputSurface, ContextCompat.getMainExecutor(this)) { result ->
-                    // The result can be processed as needed.
-                }
+                request.provideSurface(inputSurface, ContextCompat.getMainExecutor(this)) { result -> }
             }
 
-            // We form the CameraSelector by analogy with the code from the second script
             val cameraSelector = CameraSelector.Builder()
                 .addCameraFilter { cameraInfos ->
-                    // Trying to find an external camera
                     val externalCamera = cameraInfos.find { cameraInfo ->
                         val camera2Info = Camera2CameraInfo.from(cameraInfo)
                         val lensFacing = camera2Info.getCameraCharacteristic(CameraCharacteristics.LENS_FACING)
                         lensFacing == CameraCharacteristics.LENS_FACING_EXTERNAL
                     }
                         ?: cameraInfos.find { cameraInfo ->
-                            //If there is no external camera, use the rear camera.
                             val camera2Info = Camera2CameraInfo.from(cameraInfo)
                             val lensFacing = camera2Info.getCameraCharacteristic(CameraCharacteristics.LENS_FACING)
                             lensFacing == CameraCharacteristics.LENS_FACING_FRONT
                         }
                         ?: cameraInfos.find { cameraInfo ->
-                            // If there is no rear camera, try the front camera.
                             val camera2Info = Camera2CameraInfo.from(cameraInfo)
                             val lensFacing = camera2Info.getCameraCharacteristic(CameraCharacteristics.LENS_FACING)
                             lensFacing == CameraCharacteristics.LENS_FACING_BACK
